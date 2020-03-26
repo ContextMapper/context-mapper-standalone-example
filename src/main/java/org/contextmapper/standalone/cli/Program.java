c4ae10137931ec8396a26f8e7d4d9e8fd122d2e2
João Rosa <joaoasrosa@gmail.com>
2020-03-26 11:53:23 +0100

    Read command line input

---
+++ src/main/java/org/contextmapper/standalone/cli/Program.java
@@ -0,0 +1,35 @@
+package org.contextmapper.standalone.cli;
+
+import org.apache.commons.cli.*;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+
+        Options options = new Options();
+
+        Option input = new Option("t", "type", true, "Type of generator");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option file = new Option("f", "file", true, "CML file");
+        file.setRequired(true);
+        options.addOption(file);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+        }
+
+        String generatorType = cmd.getOptionValue("type");
+        String filePath = cmd.getOptionValue("file");
+
+    }
+}
